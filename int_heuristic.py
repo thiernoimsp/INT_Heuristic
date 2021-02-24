498644aaef4734e608c2c2ebd726fcb54f9de8f9
thernoimsp <therno.ndiaye@ismp-uac.org>
2021-02-24 11:38:55 +0100

    spatial_position

--- int_heuristic.py
+++ int_heuristic.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+from collections import defaultdict
 from mip import Model, xsum, maximize, BINARY, ConstrsGenerator, CutPool
 from instance_heuristic import Instance
 from int_knapsack import Int_Knapsack
@@ -9,20 +10,24 @@ from arguments_heuristic import Arguments
 
 
 class Int_Heuristic:
-	""" 
-	This class implement the new proposed model using gurobi 
+	"""
+	This class implement the new proposed model using gurobi
 	"""
 
 	def __init__(self, inst):
 		Sol = list()
-		Sols = list()
+		Spati = list()
+		Tempo = list()
+		#Sols = list()
 		ProceededNodes = list()
 		FinishedNodes = list()
-		CollectedItems = {}
+		CollectedItems = defaultdict(list)
 
 		self.inst = inst
 		self.Sol = Sol
-		self.Sols = Sols
+		self.Spati = Spati
+		self.Tempo = Tempo
+		#self.Sols = Sols
 		self.ProceededNodes = ProceededNodes
 		self.FinishedNodes = FinishedNodes
 		self.CollectedItems = CollectedItems
@@ -32,8 +37,8 @@ class Int_Heuristic:
 
 
 	def Int_Collecte_Heuristic(self):
-		for d in inst.D:
-			self.CollectedItems[d] = []
+		#for d in inst.D:
+		#	self.CollectedItems[d] = []
 
 		for f in inst.F:
 			for d in inst.path[f]:
@@ -48,12 +53,16 @@ class Int_Heuristic:
 						V_dd.pop(key)
 
 					y = Int_Knapsack(f,d, V_dd, inst.Kf)
+					if len(y[1]) != 0:
+						self.Sol.append(y)
 
 					for item in y[1]:
 						self.CollectedItems[d].append(item)
 				else:
 					y = Int_Knapsack(f,d, inst.V_d, inst.Kf)
-					
+					if len(y[1]) != 0:
+						self.Sol.append(y)
+
 					for item in y[1]:
 						self.CollectedItems[d].append(item)
 					self.ProceededNodes.append(d)
@@ -61,7 +70,7 @@ class Int_Heuristic:
 				size_col_items = list()
 				for item in y[1] :
 					size_col_items.append(inst.V_d[item])
-				
+
 				n_cap = inst.Kf[f] - sum(size_col_items)
 				n_Kf = {f:n_cap}
 				inst.Kf.update(n_Kf)
@@ -74,12 +83,20 @@ class Int_Heuristic:
 
 				#print(size_not_col_items)
 				#print(size_col_items)
-				
 
-				if inst.Kf[f] < min(size_not_col_items):
+				if len(size_not_col_items) == 0:
 					break
+				elif len(size_not_col_items) >= 1:
+					if inst.Kf[f] < min(size_not_col_items):
+						break
 
-			self.Sol.append(y)
+
+
+
+				#if inst.Kf[f] < min(size_not_col_items):
+				#	break
+
+			#self.Sol.append(y)
 
 		#print(self.ProceededNodes)
 		#print(self.FinishedNodes)
@@ -92,21 +109,23 @@ class Int_Heuristic:
 
 		for m in inst.M:
 			for d in inst.D:
-				for v in self.CollectedItems[d]:
-					if v in inst.R[m]:
-						ss = (m,d,[v])
-						self.Sols.append(ss)
+				for P in range(len(inst.Rs[m])):
+					if set(inst.Rs[m][P]).issubset(self.CollectedItems[d]):
+						ss = (m,d,P, inst.Rs[m][P])
+						self.Spati.append(ss)
 						#print(ss)
 
-				if set(inst.R[m]).issubset(self.CollectedItems[d]):
-					s = (m,d, inst.R[m])
-					self.Sols.append(s)
-					#print(s)
+		for m in inst.M:
+			for P in range(len(inst.Rt[m])):
+				if inst.HH[P] > inst.TT[P]:
+					tt = (m,P,inst.Rt[m][P])
+					self.Tempo.append(tt)
 
-		
+
+
 		print(len(inst.Rs[m]))
 		print(inst.R)
-		for i,j in enumerate(self.Sols):
+		for i,j in enumerate(self.Spati):
 			print(i,j)
 		#print(self.Sols)
 
@@ -116,7 +135,9 @@ class Int_Heuristic:
 
 		#print(sum_col)
 		print("Collected items :", sum(sum_col))
-		print("spatial req", len(self.Sols))
+		print("temporal req", len(self.Tempo))
+		print("spatial req", len(self.Spati))
+		print("objective", len(self.Tempo) + len(self.Spati))
 		#print(len(sum_col))
 
 
